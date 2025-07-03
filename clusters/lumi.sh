8f803c6047a5c5443d5b969fa7a09b532320a1c9
timurcarstensen <timurcarstensen@gmail.com>
2025-07-03 18:26:24 +0200

    feat: adding lumi

--- clusters/lumi.sh
+++ clusters/lumi.sh
@@ -1,9 +1,9 @@
-export BASE_DIR="/p/data1/mmlaion/oellm_shared_evals"
+export BASE_DIR="/pfs/lustrep4/projappl/project_462000963/evals"
 export HF_HOME="${BASE_DIR}/hf_data"
 export EVAL_OUTPUT_DIR="${BASE_DIR}/${USER}"
 export EVAL_VENV_DIR="${BASE_DIR}/.venv"
-export DEFAULT_PARTITION="standard"
-export EVAL_ACCOUNT="AIFAC_L01_028"
+export DEFAULT_PARTITION="dev-g"
+export EVAL_ACCOUNT="project_462000963"
 export EVAL_TIME_LIMIT="00:30:00"
 export NUM_GPU_PER_NODE=1
 export UV_LINK_MODE="copy"

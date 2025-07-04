b39d617ef069a74b10d37d8a722d6f1e71679f9a
David  Salinas <david.salinas.pro@gmail.com>
2025-07-04 11:18:39 +0200

    small polishing, make instructions copy-pastable, fix account command, use DEFAULT_ACCOUNT instead of EVAL_ACCOUNT

--- clusters/leonardo.sh
+++ clusters/leonardo.sh
@@ -2,4 +2,5 @@
 
 export EVAL_BASE_DIR="/leonardo_work/EUHPC_E03_068/shared_evals"
 export DEFAULT_PARTITION="boost_usr_prod"
-export EVAL_ACCOUNT="AIFAC_L01_028"
+export DEFAULT_ACCOUNT="AIFAC_L01_028"
+export NUM_GPU_PER_NODE=4
--- clusters/lumi.sh
+++ clusters/lumi.sh
@@ -2,4 +2,4 @@
 
 export EVAL_BASE_DIR="/pfs/lustrep4/projappl/project_462000963/evals"
 export DEFAULT_PARTITION="dev-g"
-export EVAL_ACCOUNT="project_462000963"
+export DEFAULT_ACCOUNT="project_462000963"
